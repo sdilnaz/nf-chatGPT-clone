596cb7e2476ce9371cc348f13d6b01adcb3f2b68
sdilnaz <dilnaz.sembekova@gmail.com>
2024-06-24 17:52:43 +0500

    fix bugs

--- backend/src/global-router.ts
+++ backend/src/global-router.ts
@@ -1,13 +1,8 @@
 import { Router } from 'express';
 import { getChatById, getChats, generateResponse } from './chat/chat.controller';
-//import { roadmapRouter } from './roadmap/roadmap.router';
-// other routers can be imported here
-
 
 const globalRouter = Router();
 
-// Use the userRouter for user-related routes
-//globalRouter.use(roadmapRouter);
 globalRouter.get('/chats', getChats);
 globalRouter.get('/chats/:id', getChatById);
 globalRouter.post('/generate', generateResponse);
